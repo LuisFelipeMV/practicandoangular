733a72caa8244d4f10712b3efaaba11b85268ff4
LuisMiranda <felipemv612@gmail.com>
2025-02-24 03:02:39 -0500

    subiendo la carpeta angular curso

---
+++ app-angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  //imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Hola este emnsaje se edito y se subio a git';
+}
