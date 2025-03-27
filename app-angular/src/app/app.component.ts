e9f242d61595a3df598488b464c47f1e32b43b82
LuisMiranda <felipemv612@gmail.com>
2025-03-27 00:23:15 -0500

    confirmado

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hola este emnsaje se edito y se subio a git';
+  title = 'Hola este es un message se edits y se subio a git';
 }
